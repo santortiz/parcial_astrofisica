8f6743e73c62028a63c7537224fad983e28efd27
Santiago <Santiago.ortiz2@udea.edu.co>
2022-02-17 10:37:13 -0500

    intento de realización punto 4

--- punto4/estructuras.c
+++ punto4/estructuras.c
@@ -1,27 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+
+struct random{
+        int step;
+        struct random *pointer;
+    };
+    
+typedef struct random Random;
+typedef Random *pointerRandom; 
+
+
+int choose();
+void createNewNodo();
+int walk();
 
 
 int main(){
 
-    struct persona{
-        char *firstName;
-        char *lastName;
-        int age;
-    } persona1, persona2;
+    Random nodo;
+    nodo.step= choose();
 
-    persona1.firstName= "Jorge";
+    int sum, lim;
 
-    printf("%s \n", persona1.firstName);
+    sum = walk(&nodo);
 
-    typedef struct persona persona;
-
-    persona persona3;
-
-    persona3.firstName= "Sergio";
-
-    printf("%s \n", persona3.firstName);
+    printf("%d", sum);
 
     return 0;
+};
+
+
+//choose() devuelve aleatoriamente -1 o 1.
+
+int choose(){
+
+    int choices[]= {-1,1};
+
+    __time_t t;
+
+    srand((unsigned) time(&t));
+
+    return choices[rand()%2];
+};
+
+void createNewNodo(pointerRandom *pointer){
+
+    pointerRandom newPointer;
+    pointerRandom previousPointer;
+    pointerRandom currentPointer;
+
+
+    newPointer = malloc(sizeof(Random));
+
+    if (newPointer != NULL){
+        newPointer-> step = choose();
+        newPointer->pointer = NULL;
+
+        previousPointer = NULL;
+        currentPointer = *pointer;
+
+        if (previousPointer == NULL){
+            newPointer->pointer= *pointer;
+            *pointer = newPointer;
+
+        } else{
+            previousPointer->pointer = newPointer;
+            newPointer->pointer= currentPointer;
+        };
+    } else {
+
+        printf("No hay memoria disponible");
+    };
+};
+
+int walk(pointerRandom currentPointer){
     
+    int sum;
+    sum= 0;
+
+    while (currentPointer!= NULL){
+        sum+= currentPointer->step;
+    };
+
+    return sum;
 };
